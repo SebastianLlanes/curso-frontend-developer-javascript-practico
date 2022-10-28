1d12408b37541af175b810543b3bcc46a1a0beff
Seba Llanes <sebastianllanessaavedra@gmail.com>
2022-10-28 17:29:55 -0300

    Integracion y modificaciones de los menues y sus interacciones

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
+  const isAsideClose = aside.classList.contains('inactive');
+  
+  if (!isAsideClose) {
+    aside.classList.add('inactive');
+  }
+  
+    desktopMenu.classList.toggle('inactive');
+
 }
 
 function toggleMobileMenu() {
-    console.log('click')
+  const isAsideClose = aside.classList.contains('inactive');
+ 
+  if (!isAsideClose) {
+    aside.classList.add('inactive');
+  }
     mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCarritoAside() {
+const isMobileMenuClose = mobileMenu.classList.contains('inactive');
+const desktopMenuClose = desktopMenu.classList.contains('inactive');
+  
+if (!isMobileMenuClose) {
+      mobileMenu.classList.add('inactive')
+
+  }  else if (!desktopMenuClose) {
+    desktopMenu.classList.toggle('inactive');
+  }
+     
+     aside.classList.toggle('inactive')
+
+} 
